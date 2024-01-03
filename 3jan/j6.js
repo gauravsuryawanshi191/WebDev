01156a4ba16dc4309fe4e07ed02476062cec4502
Gaurav <gauravsuryawanshi191@gmail.com>
2024-01-03 23:59:07 +0530

    jan3.2

---
+++ 3jan/j6.js
@@ -0,0 +1,5 @@
+console.log(1);
+console.log(2);
+setTimeout(function(){console.log(3);},2000)
+console.log(4);
+console.log(5);
---
+++ 3jan/j8.js
@@ -0,0 +1,7 @@
+var arr=["Surya","Saurabh","Shubham","Vaibhav"]
+console.log(arr)
+
+for(ele of arr){
+    console.log(ele);
+    console.log(ele.length);
+}
